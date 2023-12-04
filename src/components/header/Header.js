3df9f0a68abd5d2b88decfdd16107e3549eedd9a
Munsa1 <munsamibenge65@gmail.com>
2023-12-05 00:59:57 +0200

    Add href attribute to links

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -3,17 +3,17 @@ import React from 'react';
 function Header() {
   return (
     <div>
-        <nav className='logo'>
-            <img src='' />
-        </nav>
-        <nav className='links'>
-            <ul>
-                <a><li>Home</li></a>
-                <a><li>Resume</li></a>
-                <a><li>Projects</li></a>
-                <a><li>Contact</li></a>
-            </ul>
-        </nav>
+      <nav className="logo">
+        <img src="#" />
+      </nav>
+      <nav className="links">
+        <ul>
+          <a href="#"><li>Home</li></a>
+          <a href="#"><li>Resume</li></a>
+          <a href="#"><li>Projects</li></a>
+          <a href="#"><li>Contact</li></a>
+        </ul>
+      </nav>
     </div>
   );
 }
